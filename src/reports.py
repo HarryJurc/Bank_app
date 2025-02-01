b5ea0d32bd98d19802319f9bb1301b22ab110830
Igor Gurchenko <igorivanov122@gmail.com>
2025-02-01 21:35:52 +0300

    Fixes

--- src/reports.py
+++ src/reports.py
@@ -1,20 +1,21 @@
-import pandas as pd
 import datetime
 import json
-from typing import Optional, Callable
-import os
 import logging
+import os
+from typing import Callable, Optional, Any, TextIO
+
+import pandas as pd
 
 # Настройка логирования
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-def save_report(filename: Optional[str] = None) -> Callable:
-    def decorator(func: Callable) -> Callable:
-        def wrapper(*args, **kwargs):
+def save_report(filename: Optional[str] = None) -> Callable[[Callable[..., pd.DataFrame]], Callable[..., pd.DataFrame]]:
+    def decorator(func: Callable[..., pd.DataFrame]) -> Callable[..., pd.DataFrame]:
+        def wrapper(*args: Any, **kwargs: Any) -> pd.DataFrame:
             result = func(*args, **kwargs)
             # Преобразование столбца с датами в строковый формат
-            result["Дата операции"] = result["Дата операции"].dt.strftime("%Y-%m-%d %H:%M:%S")
+            result["Дата операции"] = result["Дата операции"].dt.strftime("%Y-%m-%d %H:%М:%С")
             if filename:
                 file = filename
             else:
@@ -22,9 +23,7 @@ def save_report(filename: Optional[str] = None) -> Callable:
             with open(file, 'w', encoding='utf-8') as f:
                 json.dump(result.to_dict(orient='records'), f, ensure_ascii=False, indent=2)
             return result
-
         return wrapper
-
     return decorator
 
 
@@ -35,7 +34,7 @@ def get_operations_data(filepath: str = "../data/operations.xlsx") -> pd.DataFra
 
     df = pd.read_excel(filepath)
     df.columns = df.columns.str.strip()
-    df["Дата операции"] = pd.to_datetime(df["Дата операции"], errors='coerce', dayfirst=True)
+    df["Дата операции"] = pd.to_datetime(df["Дата операции"], errors="coerce", dayfirst=True)
     logging.info(f"Загружено строк: {len(df)}")
     logging.info(f"Пример данных: {df.head()}")
     logging.info(f"Уникальные категории: {df['Категория'].unique()}")
@@ -53,8 +52,11 @@ def spending_by_category(transactions: pd.DataFrame, category: str, date: Option
     start_date = end_date - datetime.timedelta(days=90)
     logging.info(f"Фильтрация данных с {start_date} по {end_date} для категории {category}")
 
-    mask = (transactions["Дата операции"] >= start_date) & (transactions["Дата операции"] <= end_date) & (
-            transactions["Категория"] == category)
+    mask = (
+        (transactions["Дата операции"] >= start_date)
+        & (transactions["Дата операции"] <= end_date)
+        & (transactions["Категория"] == category)
+    )
     filtered_transactions = transactions[mask]
 
     logging.info(f"Найдено строк: {len(filtered_transactions)}")
